a77868f7a07ca1eb76e1aa06f2284c3ab29b7f55
ramfaster <n1001ship@nate.com>
2020-04-23 17:57:08 +0900

    [0423_2 Update]

--- terms/word.py
+++ terms/word.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import re, json
 #from math import ceil
 from module import dbModule
+from flask_paginate import Pagination, get_page_parameter
 
 if __name__ == "__main__":
     app.debug = True
@@ -16,19 +17,7 @@ def list():
     print("#### terms list init ####")
     str_url = request.referrer
     print("str_url : ", str_url)
-    strs = re.findall('list/(\w+)', str_url)
-    num = 10
-    #num = request.form.get('rows')
-    s_page = 0
-    page = 0
-    if len(strs) > 0:
-        page = int(strs[0])
-    else:
-        page = 1
-
-    if page > 1:
-        s_page = page - 1
-    n_page = page
+    page = request.args.get(get_page_parameter(), type=int, default=1)
    
     # if request.method == 'GET' and 'searchKey' in request.form and 'searchValue' in request.form:
     # Create variables for easy access
@@ -43,31 +32,23 @@ def list():
         if searchKey == 'all' or searchKey == None:
             sql = "SELECT WF.KORNM, WF.ENG_ABRV, WFD.ENG_MEAN, WF.WRD_TY, WFD.DESCR, WFD.SYNONYM FROM TB_WRD_FOAFT WF, TB_WRD_FOAFT_DETL WFD WHERE WF.WRD_ID = WFD.WRD_ID AND WF.KORNM"
             totalRowCount = db_class.executeRowCount(sql)
-            row = db_class.executeAll(sql)            
+            words = db_class.executeAll(sql)            
         elif searchKey == 'korNm':
             sql = "SELECT WF.KORNM, WF.ENG_ABRV, WFD.ENG_MEAN, WF.WRD_TY, WFD.DESCR, WFD.SYNONYM FROM TB_WRD_FOAFT WF, TB_WRD_FOAFT_DETL WFD WHERE WF.WRD_ID = WFD.WRD_ID AND WF.KORNM LIKE '%s'||'%'"%(searchValue)
             totalRowCount = db_class.executeRowCount(sql)
-            row = db_class.executeAll(sql)
+            words = db_class.executeAll(sql)
         elif searchKey == 'engAbrv':
             sql = "SELECT WF.KORNM, WF.ENG_ABRV, WFD.ENG_MEAN, WF.WRD_TY, WFD.DESCR, WFD.SYNONYM cnt FROM TB_WRD_FOAFT WF, TB_WRD_FOAFT_DETL WFD WHERE WF.WRD_ID = WFD.WRD_ID AND WF.ENG_ABRV LIKE '%s'||'%'"%(searchValue)
             totalRowCount = db_class.executeRowCount(sql)
-            row = db_class.executeAll(sql)
+            words = db_class.executeAll(sql)
     except Exception as e:
         print(e)
 
     print("#2-1 totalRowCount>>> : ", totalRowCount)
     total_rows = totalRowCount
-    total_page = int(11/10) 
-    #total_page = int(ceil(total_rows / int(num))) 
-    if n_page > total_page:
-        n_page = total_page
-    #########################################################################
-    dataset = datalist[s_page * int(num):n_page * int(num)]
+    pagination = Pagination(page=page, total=totalRowCount, search=search, record_name='words')
     
-    row_array =[]
-    rows = len(dataset)
-    
-    for index in range(0,rows):
+    for index in range(0,words):
         dic_data ={}
         #dic_data["no"]=str(index)
         dic_data["KORNM"] = dataset[index]
@@ -78,14 +59,13 @@ def list():
         dic_data["DESCR"] = dataset[index]
         row_array.append(dic_data)
     context={
-        "page":"1",
-        "total":str(rows),
-        "records":str(rows),
-        "rows":row_array
+        "page":page,
+        "pagination":pagination,
+        "total":totalRowCount,
+        "records":str(words),
+        "words":row_array
     }
     
-    #return render_template('terms/list.html')
-    #return jsonify(context)
     return Response(json.dumps(context), mimetype='application/json')
     
 @terms_bp.route('/register', methods=['GET', 'POST'])
